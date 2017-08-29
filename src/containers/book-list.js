9d8b6094f01b77c77b024376a1dce3d4d754f60f
Thilina Ratnayake <thilina@tratnayake.me>
2017-08-28 22:05:36 -0700

    Transition from mac to PC

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class BookList extends Component {
+  debugger;
+  renderList(){
+    return this.props.books.map((book) => {
+      //console.log(book);
+      return (
+         <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  //Whatever gets returned will show up as props
+  //inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+
+export default connect(mapStateToProps)(BookList);
